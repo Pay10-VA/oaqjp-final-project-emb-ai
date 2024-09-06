a30ca54a7e32ab9a43ea912036acb6e0002756cd
Payton <paytonviloria@gmail.com>
2024-09-06 16:41:30 -0400

    Completed lab

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -2,12 +2,24 @@ import requests
 import json
 
 def emotion_detector(text_to_analyse):
+    """ calls ai api"""
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     myobj = { "raw_document": { "text": text_to_analyse } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header)
 
     regularResponse = json.loads(response.text)
+
+    if response.status_code == 400:
+        dictionary = {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+        return dictionary
 
     beforeDictionary = {
         'anger': regularResponse['emotionPredictions'][0]['emotion']['anger'],
